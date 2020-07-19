18275e2cc7e4dedfcf34f2549932742597453643
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2020-07-19 08:55:02 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,7 +12,7 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
-
+from .modules import authorize, list
 
 @run_async
 def stats(update, context):
